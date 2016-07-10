7d93f4b0529186602aced520af8a74d821089d68
Yaroslav Putria <yaroslav.putria@gmail.com>
2016-07-10 15:37:03 +0300

    homework lesson6 DOM

---
+++ lesson6/bizzare.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+	###Задание 1 - необязательное:
+	You need to find all values of attribute named "base64" in bizzare.html.
+	After that make one long string and decode it. Mind the name of attribute. Follow instructions.
+*/
+
+var allEl = document.querySelectorAll("[base64]"),
+	oneLongString = [].map.call(allEl, function(item) {
+ 		return item.getAttribute("base64");
+ 	}).join("");
+
+var decodedRes = atob(oneLongString);
+
+console.log(decodedRes);
+
+//Create and execute function like: Function(string),
+//where string is concatenated value of all Comment nodes from this document
+
+
+var re = /<!--[\s\S]*?-->/g,
+	allText = document.body.innerHTML,
+	allComments = allText.match(re);
+
+var funcText = allComments.map(function(item) {
+	return item.slice(4, (item.length - 3));
+}).join("");
+
+eval(funcText);
+
